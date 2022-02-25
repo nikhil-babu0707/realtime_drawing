96fa7eab03a29905cf1c412221ab6bc017e3cb04
nikhil-babu0707 <87303388+nikhil-babu0707@users.noreply.github.com>
2022-02-25 16:34:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-noseX = 0;
-noseY = 0;
-difference = 0;
-rightWristX = 0;
-leftWristX = 0;
+let noseX = 0;
+let noseY = 0;
+let difference = 0;
+let rightWristX = 0;
+let leftWristX = 0;
 
 
 
 function setup() {
-    video = createCapture(VIDEO);
+    let video = createCapture(VIDEO);
     video.size(550, 500);
 
-    canvas = createCanvas(550, 500);
+    let canvas = createCanvas(550, 500);
     canvas.position(560, 150);
 
-    PoseNet = ml5.poseNet(video, modelloaded);
-    PoseNet.on('pose', gotPoses);
+    let poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function modelloaded() {
@@ -29,7 +29,7 @@ function gotPoses(results) {
         console.log("noseX = " + noseX + "noseY = " + noseY);
 
         leftWristX = results[0].pose.leftWrist.x;
-        rightWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
         difference = floor(leftWristX - rightWristX);
 
         console.log("left wrist x = " + leftWristX + "right wrist x = " + rightWristX + "difference = " + difference);
@@ -40,6 +40,6 @@ function draw() {
     background('#969A97');
     document.getElementById("square_side").innerHTML = "width and height of the square is : " + difference + "px";
     fill('#F90093');
-    stroke('#A90231');
+    stroke('#F90093');
     square(noseX, noseY, difference);
 }
